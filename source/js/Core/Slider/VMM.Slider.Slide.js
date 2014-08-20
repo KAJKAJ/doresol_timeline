1b4dd01b5e183c71feab493e96e4c1d84b5cbae0
SunJang <realjangsun@gmail.com>
2014-08-20 15:04:48 +0900

    passing story object to angular

--- source/js/Core/Slider/VMM.Slider.Slide.js
+++ source/js/Core/Slider/VMM.Slider.Slide.js
@@ -259,7 +259,7 @@ if (typeof VMM.Slider != 'undefined') {
         if (data.type == "start") {
           c.text    = VMM.createElement("div", c.text, "container");
         } else {
-          c.text    = VMM.createElement("story-detail", null, null, "story-key="+data.story.$id+" add-comment='addComment()'");
+          c.text    = VMM.createElement("story-detail", null, null,"story=\"storiesObject[\'"+data.story.ref_era+"\'][\'"+data.story.$id+"\']\" add-comment=\"addComment(comment)\"");
         }
         $text   = VMM.appendAndGetElement($slide, "<div>", "text", VMM.TextElement.create(c.text));  
 			}
